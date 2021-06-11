ba661d12ef17b218396852a970a3bac7d638c7d1
rahat854 <rahatkabir20@gmail.com>
2021-06-11 23:47:09 +0600

    chapter 2

---
+++ chapter2/hello.js
@@ -0,0 +1,8 @@
+console.log('Hello world')
+console.log('I love Bangladesh')
+console.log(45)
+console.log(60.785)
+
+console.log('My Fav No: ' + 13)
+console.log(9 + 9)
+console.log('9' + 9)
---
+++ chapter2/variable.js
@@ -0,0 +1,6 @@
+let Name = 'Rahat Kabir Dhrubo'
+let age = 23;
+console.log(Name + ' knows Js');
+console.log('his age is only ' + age);
+let mathNumber = 10;
+let accountNumberDetailsId = 104;
