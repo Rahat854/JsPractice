fb7762276f43ce3f68b6164a6312bf7655576fd4
rahat854 <rahatkabir20@gmail.com>
2021-06-14 01:24:56 +0600

    chapter 6 completed

---
+++ chapter6/method.js
@@ -0,0 +1,7 @@
+let arr = [4,5,9,6]
+let arr2 =[ 7, 8, 0, 4]
+console.log(arr.join('|'))
+console.log(arr.fill(0))
+console.log(arr.concat(arr2))
+console.log(Array.isArray(arr))
+console.log(Array.from(arr))
--- chapter6/multiarr.js
+++ chapter6/multiarr.js
@@ -1,1 +1,14 @@
 //Multi dimensional Array
+let arr = [
+    [78, 72, 80,90],
+    [78, 72, 80,90],
+    [78, 72, 80,90]
+
+]
+// console.log(arr[0][0])
+// console.log(arr[1][1])
+for(let i =0; i<arr.length;i++){
+    for(let j = 0; j < arr[i].length; j++){
+        console.log('Element ' + i + ' : ' + arr[i][j])
+    }
+}
