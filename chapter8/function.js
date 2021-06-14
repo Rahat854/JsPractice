753c9617b80c53d3c77ef73f8b956f73c93b8337
rahat854 <rahatkabir20@gmail.com>
2021-06-14 23:54:51 +0600

    function define

--- chapter8/function.js
+++ chapter8/function.js
@@ -2,5 +2,20 @@
 //what is function
 
 //Input Output Processing
-let date = new Date()
-date.getFullYear() 
+// let date = new Date()
+// date.getFullYear() 
+
+function functionName() {
+    
+} 
+
+function add(){
+    let a = 10
+    let b = 20
+    console.log(a+b)
+}
+function sub(){
+    let a = 50
+    let b = 20
+    console.log(a-b)
+}
