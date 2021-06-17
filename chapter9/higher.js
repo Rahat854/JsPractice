42e03e816b992b7197e4194e58d25f9dc031e53f
rahat854 <rahatkabir20@gmail.com>
2021-06-18 02:47:19 +0600

    higher order function

---
+++ chapter9/higher.js
@@ -0,0 +1,19 @@
+function add(a,b) {
+    return a+b
+}
+
+function manipulate(a,b, func) {
+    let c = a + b
+    let d = a - b
+
+    // function multiply() {
+    //     let m = func(a, b)
+    //     return c*d*m
+    // }
+    return function () {
+        let m = func(a, b)
+        return c*d*m
+    }
+}
+let multiply = manipulate(3, 4, add)
+console.log(multiply())
