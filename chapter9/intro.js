eb57906d9d117886abd6f223effcc45c37ee032a
rahat854 <rahatkabir20@gmail.com>
2021-06-18 02:21:11 +0600

    intro

---
+++ chapter9/intro.js
@@ -0,0 +1,14 @@
+//chapter Nine: Functional Programming with Javascript
+
+// What is functional Programming?
+`Functional programming is a programming paradigm-a style of building
+the structure and elements of computer programs-that treats computation as the
+evaluation of mathematical functions and avoids changing-state and mutable 
+data-Wikipedia`
+//Three main Terms
+    `   1. Pure Functions`
+    `   2. First Class Functions`
+    `   3. Higher order functions`
+
+    
+    `*********** Function Acts like a value *********`
