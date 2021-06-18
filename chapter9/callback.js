050119c2772620991f9fcb2f26bfeb374fb2c018
rahat854 <rahatkabir20@gmail.com>
2021-06-18 21:15:28 +0600

    callback

---
+++ chapter9/callback.js
@@ -0,0 +1,24 @@
+function sample(a, b, cb) {
+    let c = a + b
+    let d = a - b
+    let result = cb(c, d)
+    return result
+}
+
+function sum (a, b) {
+    return a+b
+}
+let result = sample(5, 8, sum)
+console.log(result)
+
+let result2 = sample(5,8, function(c, d) {
+    return c-d
+})
+
+console.log(result2)
+
+let result3 = sample(5, 8, function(c, d) {
+    return c * d
+})
+
+console.log(result3)
