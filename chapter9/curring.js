31b06bd775db5474b07edf375fbc1f9cb953d7e5
rahat854 <rahatkabir20@gmail.com>
2021-06-19 00:01:54 +0600

    curring

---
+++ chapter9/curring.js
@@ -0,0 +1,15 @@
+function add(a, b, c) {
+    return a + b + c
+}
+//add(41, 52, 63)
+
+function curring(a) {
+    return function(b) {
+        return function(c) {
+            return a + b + c
+        }
+    }
+}
+
+let result = curring(5)(10)(15)
+console.log(result)
