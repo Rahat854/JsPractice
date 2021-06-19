ad797b44e5e148a6ba5d2c259c7a207f8a1b2a60
rahat854 <rahatkabir20@gmail.com>
2021-06-19 21:21:35 +0600

    context

---
+++ chapter10/context.js
@@ -0,0 +1,28 @@
+function a() {
+    b()
+    console.log('I am a function')
+}
+
+function b() {
+    d()
+    console.log('I am b function')
+}
+
+function c() {
+    console.log('I am c function')
+}
+
+function d() {
+    c()
+    console.log('I am d function')
+}
+
+let x = 100
+
+a()
+console.log('I am Global')
+
+//Execution of D function context
+//execution OF B function context
+//Execution of A function context
+//Execution of global context
