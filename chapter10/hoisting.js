c0c87798b0a8ae9873e80fa8a02b1880dbff06fa
rahat854 <rahatkabir20@gmail.com>
2021-06-20 01:22:29 +0600

    hoisting

---
+++ chapter10/hoisting.js
@@ -0,0 +1,24 @@
+let a =100
+// newPrint(a)
+
+print(10)
+
+let newPrint = print
+
+newPrint(45)
+
+function print(a) {
+    console.log(a)
+}
+
+
+print(a)
+
+//Creational Phase
+// a = undefined
+// newprint = undefined
+//print =ref
+
+//Executional Phase
+// a = 100
+// newprint = reference
