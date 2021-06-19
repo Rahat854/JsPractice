9d55ae8557df27329534fa0a286810f31853e212
rahat854 <rahatkabir20@gmail.com>
2021-06-19 14:03:42 +0600

    compiled_vs_interpreted

---
+++ chapter10/compiled_vs_interpreted.js
@@ -0,0 +1,15 @@
+// JavaScript Compiled vs Interpreted
+
+function a() {
+    console.log('I am a function')
+}
+
+function b() {
+    console.log('I am b function')
+}
+
+a()
+
+b()
+
+// JIT Just In Time Compiler
