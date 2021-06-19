ca57488270707a97e845d285d689447c6b491b75
rahat854 <rahatkabir20@gmail.com>
2021-06-20 01:51:13 +0600

    nested-scope

--- chapter10/scope.js
+++ chapter10/scope.js
@@ -1,8 +1,15 @@
 let x = 55
 
 function test() {
-    // let x = 45
-    console.log(x)
+    let x = 45
+    // console.log(x)
+
+    function nested() {
+        // let y = 65
+        console.log(x)
+    }
+    // console.log(y)
+    nested()
 }
 
 test()
