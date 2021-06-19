8b4d6591e5a9074c31e6b29ced165b3e8b97b704
rahat854 <rahatkabir20@gmail.com>
2021-06-20 01:03:45 +0600

    vaariable

---
+++ chapter10/variable.js
@@ -0,0 +1,22 @@
+let a =10
+
+let b
+
+console.log(a)
+
+console.log(b)
+
+b = 20
+
+console.log(c)
+var c = 30
+console.log(c)
+
+// Creational phase
+// a = undefined
+// b = undefined
+// c = undefined
+
+// Executional phase
+//a = 10
+//b = 20
