0cee95fa6da006aa24a7fa26e011bd13c526cba0
rahat854 <rahatkabir20@gmail.com>
2021-06-20 23:25:29 +0600

    update

--- chapter12/index.js
+++ chapter12/index.js
@@ -35,24 +35,41 @@
  //  another.print()
 
 
-let createRect = function(width, height) {
-    return {
-        width : width,
-        height : height,
+// let createRect = function(width, height) {
+//     return {
+//         width : width,
+//         height : height,
     
-        draw: function() {
-            console.log('I am a rectangle')
-            this.printProperties()
-            console.log(this)
-        },
-        printProperties : function() {
-            console.log('My width is ' + this.width)
-            console.log('My height is ' + this.height)
-        }
+//         draw: function() {
+//             console.log('I am a rectangle')
+//             this.printProperties()
+//             console.log(this)
+//         },
+//         printProperties : function() {
+//             console.log('My width is ' + this.width)
+//             console.log('My height is ' + this.height)
+//         }
+//     }
+// }
+// let rect1 = createRect(10,8)
+// rect1.draw()
+// let rect2 = createRect(29, 74)
+// rect2.draw()
+
+let Rectangle = function (width, height) {
+    this.width = width
+    this.height = height
+
+    this.draw = function() {
+        console.log('I am a rectangle')
+        this.printProperties()
+        console.log(this)
+    }
+    this.printProperties = function() {
+        console.log('My width is ' + this.width)
+        console.log('My height is ' + this.height)
     }
 }
-let rect1 = createRect(10,8)
-rect1.draw()
-let rect2 = createRect(29, 74)
-rect2.draw()
 
+let rect3 = new Rectangle(10, 8)
+rect3.draw()
