7cd9760717140a23efb5843648b47e40520eea4f
rahat854 <rahatkabir20@gmail.com>
2021-06-20 23:12:30 +0600

    update

--- chapter12/index.js
+++ chapter12/index.js
@@ -1,16 +1,80 @@
-let rect = {
+ // let rect = {
+ //     width : 100,
+ //     height : 50,
+ 
+ //     draw: function() {
+ //         console.log('I am a rectangle')
+ //         this.printProperties()
+ //         console.log(this)
+ //     },
+ //     printProperties : function() {
+ //         console.log('My width is ' + this.width)
+ //         console.log('My height is ' + this.height)
+ //     }
+ // }
+ 
+ // rect.draw()
+ 
+ // // rect.height = 100
+ 
+ // // rect.draw()
+ 
+ // function myFunc() {
+ //     console.log(this)
+ //     rect.printProperties()
+ // }
+ 
+ //  new myFunc()
+ 
+ //  let another = {
+ //      width : 47,
+ //      height : 56,
+ //      print : rect.printProperties
+ //  }
+ //  console.log(another.print)
+ //  another.print()
+
+ let rect = {
     width : 100,
     height : 50,
 
     draw: function() {
         console.log('I am a rectangle')
+        this.printProperties()
+        console.log(this)
+    },
+    printProperties : function() {
         console.log('My width is ' + this.width)
         console.log('My height is ' + this.height)
     }
 }
 
-rect.draw()
+let rect2 = {
+    width : 10,
+    height : 50,
 
-rect.height = 100
+    draw: function() {
+        console.log('I am a rectangle')
+        this.printProperties()
+        console.log(this)
+    },
+    printProperties : function() {
+        console.log('My width is ' + this.width)
+        console.log('My height is ' + this.height)
+    }
+}
 
-rect.draw()
+let rect3 = {
+    width : 10,
+    height : 5,
+
+    draw: function() {
+        console.log('I am a rectangle')
+        this.printProperties()
+        console.log(this)
+    },
+    printProperties : function() {
+        console.log('My width is ' + this.width)
+        console.log('My height is ' + this.height)
+    }
+}
