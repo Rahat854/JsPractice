b1c0343eeb6a027211a56a725873631bf2895a88
rahat854 <rahatkabir20@gmail.com>
2021-06-22 01:39:10 +0600

    constructor

--- chapter13/index.js
+++ chapter13/index.js
@@ -1,14 +1,17 @@
 //Inheritance
 
-// function Person(name) {
-//     this.name = name
-// }
-// let p1 = new Person('Rahat Kabir')
-// console.log(p1)
-
-var person = {
-    name : 'Rahat Kabir'
+function Person(name) {
+    this.name = name
 }
+let p1 = new Person('Rahat Kabir')
+console.log(p1)
+
+Person.prototype.PI = 3.1416
+let p2 = new Person('Rahat')
+console.log(p2)
+// var person = {
+//     name : 'Rahat Kabir'
+// }
 // console.log(person)
 
 // for( i in person) {
@@ -22,10 +25,12 @@ var person = {
 // let baseObj = Object.getPrototypeOf(person)
 // let descriptor = Object.getOwnPropertyDescriptor(baseObj,'toString')
 // console.log(descriptor)
-Object.defineProperty(person,'name', {
-    enumerable : false,
-    writable : false,
-    configurable : false,
-    value : 'Rahat Kabir Dhrubo'
-})
+// Object.defineProperty(person,'name', {
+//     enumerable : false,
+//     writable : false,
+//     configurable : false,
+//     value : 'Rahat Kabir Dhrubo'
+// })
 
+console.log(Object.getPrototypeOf(Person))
+console.log(Person.prototype)
