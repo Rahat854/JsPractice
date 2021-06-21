d387fc5faaf1b91de4e2af8122dc19410dbfb412
rahat854 <rahatkabir20@gmail.com>
2021-06-22 01:30:45 +0600

    property descriptor

--- chapter13/index.js
+++ chapter13/index.js
@@ -1,7 +1,31 @@
 //Inheritance
 
-function Person(name) {
-    this.name = name
+// function Person(name) {
+//     this.name = name
+// }
+// let p1 = new Person('Rahat Kabir')
+// console.log(p1)
+
+var person = {
+    name : 'Rahat Kabir'
 }
-let p1 = new Person('Rahat Kabir')
-console.log(p1)
+// console.log(person)
+
+// for( i in person) {
+//     console.log(i)
+// }
+// console.log(Object.keys(person))
+
+// let descriptor = Object.getOwnPropertyDescriptor(person,'name')
+// console.log(descriptor)
+
+// let baseObj = Object.getPrototypeOf(person)
+// let descriptor = Object.getOwnPropertyDescriptor(baseObj,'toString')
+// console.log(descriptor)
+Object.defineProperty(person,'name', {
+    enumerable : false,
+    writable : false,
+    configurable : false,
+    value : 'Rahat Kabir Dhrubo'
+})
+
