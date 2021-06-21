bda2cb5876eb033923b5cc5b5aa59d629f9fcc61
rahat854 <rahatkabir20@gmail.com>
2021-06-21 17:15:02 +0600

    abstraction

--- chapter12/index.js
+++ chapter12/index.js
@@ -147,3 +147,19 @@
 // }
 // changMe(obj)
 // console.log(obj)
+let Rectangle = function (width, height) {
+    this.width = width
+    this.height = height
+
+    this.draw = function() {
+        console.log('I am a rectangle')
+        this.printProperties()
+        console.log(this)
+    }
+    this.printProperties = function() {
+        console.log('My width is ' + this.width)
+        console.log('My height is ' + this.height)
+    }
+}
+
+let rect7 = new Rectangle(7,9)
