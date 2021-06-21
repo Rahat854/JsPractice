835eaa0d0b6fa1eeb255f77d448e4d41e841ea70
rahat854 <rahatkabir20@gmail.com>
2021-06-22 01:52:07 +0600

    instance vs proto

--- chapter13/index.js
+++ chapter13/index.js
@@ -1,36 +1,53 @@
-//Inheritance
+// //Inheritance
 
-function Person(name) {
-    this.name = name
+// function Person(name) {
+//     this.name = name
+// }
+// let p1 = new Person('Rahat Kabir')
+// console.log(p1)
+
+// Person.prototype.PI = 3.1416
+// let p2 = new Person('Rahat')
+// console.log(p2)
+// // var person = {
+// //     name : 'Rahat Kabir'
+// // }
+// // console.log(person)
+
+// // for( i in person) {
+// //     console.log(i)
+// // }
+// // console.log(Object.keys(person))
+
+// // let descriptor = Object.getOwnPropertyDescriptor(person,'name')
+// // console.log(descriptor)
+
+// // let baseObj = Object.getPrototypeOf(person)
+// // let descriptor = Object.getOwnPropertyDescriptor(baseObj,'toString')
+// // console.log(descriptor)
+// // Object.defineProperty(person,'name', {
+// //     enumerable : false,
+// //     writable : false,
+// //     configurable : false,
+// //     value : 'Rahat Kabir Dhrubo'
+// // })
+
+// console.log(Object.getPrototypeOf(Person))
+// console.log(Person.prototype)
+
+function Square(width) {
+    this.width = width
 }
-let p1 = new Person('Rahat Kabir')
-console.log(p1)
 
-Person.prototype.PI = 3.1416
-let p2 = new Person('Rahat')
-console.log(p2)
-// var person = {
-//     name : 'Rahat Kabir'
-// }
-// console.log(person)
+Square.prototype = {
+    draw :    this.draw = function() {
+        console.log('Draw')
+    },
+    toString: function() {
+        return 'My width is = ' + this.width
+    }
 
-// for( i in person) {
-//     console.log(i)
-// }
-// console.log(Object.keys(person))
+}
 
-// let descriptor = Object.getOwnPropertyDescriptor(person,'name')
-// console.log(descriptor)
-
-// let baseObj = Object.getPrototypeOf(person)
-// let descriptor = Object.getOwnPropertyDescriptor(baseObj,'toString')
-// console.log(descriptor)
-// Object.defineProperty(person,'name', {
-//     enumerable : false,
-//     writable : false,
-//     configurable : false,
-//     value : 'Rahat Kabir Dhrubo'
-// })
-
-console.log(Object.getPrototypeOf(Person))
-console.log(Person.prototype)
+let sqr1 = new Square(10)
+let sqr2 = new Square(5)
