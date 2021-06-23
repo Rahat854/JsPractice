773379b0216ffa12158132815195741eb51f63c6
rahat854 <rahatkabir20@gmail.com>
2021-06-23 22:41:10 +0600

    string template

--- chapter15/src/scripts/index.js
+++ chapter15/src/scripts/index.js
@@ -1,3 +1,16 @@
 import '../styles/index.scss'
+// let s = `Rahat
+// kabir
+//         dhrubo`
+// console.log(s.trim())
 
+let age = 13
+let name = 'Rahat kabir'
 
+console.log('My name is ' + name + ' and I am ' + age + ' years old')
+console.log(`My name is ${name} and I am ${age} years old. i am ${age < 18 ? 'not' : ''} adult`)
+
+console.log(name.padStart(15,'*'))
+
+console.log(name.padEnd(15,'a'))
+console.log('s'.repeat(23))
