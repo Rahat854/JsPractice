e0fc26a1e6bf5995bc8d10db2ae7625644e1c5bb
rahat854 <rahatkabir20@gmail.com>
2021-06-23 22:59:22 +0600

    var vs let vs const

--- chapter15/src/scripts/index.js
+++ chapter15/src/scripts/index.js
@@ -4,13 +4,30 @@ import '../styles/index.scss'
 //         dhrubo`
 // console.log(s.trim())
 
-let age = 13
-let name = 'Rahat kabir'
+// let age = 13
+// let name = 'Rahat kabir'
 
-console.log('My name is ' + name + ' and I am ' + age + ' years old')
-console.log(`My name is ${name} and I am ${age} years old. i am ${age < 18 ? 'not' : ''} adult`)
+// console.log('My name is ' + name + ' and I am ' + age + ' years old')
+// console.log(`My name is ${name} and I am ${age} years old. i am ${age < 18 ? 'not' : ''} adult`)
 
-console.log(name.padStart(15,'*'))
+// console.log(name.padStart(15,'*'))
 
-console.log(name.padEnd(15,'a'))
-console.log('s'.repeat(23))
+// console.log(name.padEnd(15,'a'))
+// console.log('s'.repeat(23))
+
+//var vs let vs const
+
+// if(true) {
+//     let a = 10
+// }
+// // console.log(a)
+
+// for(let i =  0; i< 10; i++) {
+
+// }
+// // console.log(i)
+// (function() {
+//     var abc = 'ABC'
+//     console.log(abc)
+// })
+// console.log(abc)
