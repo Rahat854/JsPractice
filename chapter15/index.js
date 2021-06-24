a863b5ae8025a625c5a5047143a7ee9731d88313
rahat854 <rahatkabir20@gmail.com>
2021-06-24 21:59:46 +0600

    generator

--- chapter15/index.js
+++ chapter15/index.js
@@ -176,10 +176,57 @@ const arr = [1, 2, 3]
 // console.log(iterateText.next())
 // console.log(iterateText.next())
 
-for(let v of arr) {
-    console.log(v)
+// for(let v of arr) {
+//     console.log(v)
+// }
+
+// for(let v of 'Rahat Kabir') {
+//     console.log(v)
+// }
+let obj = {
+    start: 1,
+    end:5,
+    // [Symbol.iterator] : function() {
+    //     let currentValue = this.start
+    //     const self = this
+    //     return {
+    //         next() {
+    //             return {
+    //                 done: currentValue > self.end,
+    //                 value: currentValue > self.end ? undefined : currentValue++
+    //             }
+    //         }
+    //     }
+    // }
+    [Symbol.iterator] : function* () {
+        let currentValue = this.start
+        while(currentValue <= this.end) {
+            yield currentValue++
+        }
+    }
 }
 
-for(let v of 'Rahat Kabir') {
-    console.log(v)
+// let iterate = obj[Symbol.iterator]()
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+
+// function* generate() {
+//     yield 1
+//     yield 2
+//     yield 3
+// }
+function* generate(collection) {
+    for (let i = 0; i < collection.length; i++) {
+        yield collection[i]
+    }
 }
+
+let it = generate(arr)
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
