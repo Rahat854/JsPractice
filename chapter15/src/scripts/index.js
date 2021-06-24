257a3e6d9a5df02fa8c01617a8e4afec59857e43
rahat854 <rahatkabir20@gmail.com>
2021-06-24 16:23:44 +0600

    distructing

--- chapter15/src/scripts/index.js
+++ chapter15/src/scripts/index.js
@@ -74,7 +74,44 @@ import '../styles/index.scss'
 // }
 // console.log(sqr())
 
-function greet(msg = 'Hello', name = 'Rahat Kabir Dhrubo') {
-    console.log(`${msg}! ${name}`)
-}
-greet()
+// function greet(msg = 'Hello', name = 'Rahat Kabir Dhrubo') {
+//     console.log(`${msg}! ${name}`)
+// }
+// greet()
+
+// function sum(... rest) {
+//     return rest.reduce((a,b) => a+b)
+// }
+
+// console.log(sum(1,2,4,3,5,32,45))
+
+// let a = [1,2,3]
+// console.log(...a)
+
+// let obj = {
+//     a: 10,
+//     b: 20,
+//     c: 30
+// }
+
+// let obj2 = {
+//     ...obj
+// }
+
+// let person = {
+//     Name : 'Rahat Kabir Dhrubo',
+//     email : 'rahatkabir20@gmail.com',
+//     adress: {
+//         city: 'Dhaka',
+//         country : 'Bangladesh'
+//     }
+// }
+
+// let {Name, email, adress: {city, country}} = person
+// console.log(Name , email, city, country)
+
+let arr = [1,2,3,4,5]
+
+let [first, second, , ,last] = arr
+console.log(first, second, last)
+
