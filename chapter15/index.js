208fae81dffc2d4ee9a336d05ca3d066594ffa34
rahat854 <rahatkabir20@gmail.com>
2021-06-24 21:37:15 +0600

    symbol.iterator

--- chapter15/index.js
+++ chapter15/index.js
@@ -160,7 +160,18 @@ const arr = [1, 2, 3]
 //     }
 // }
 // let iterate = createIterator(arr)
-// console.log(iterate.next())
-// console.log(iterate.next())
-// console.log(iterate.next())
-// console.log(iterate.next())
+
+let iterate = arr[Symbol.iterator]()
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+console.log(iterate.next())
+
+let str = 'TEXT'
+
+let iterateText = str[Symbol.iterator]()
+console.log(iterateText.next())
+console.log(iterateText.next())
+console.log(iterateText.next())
+console.log(iterateText.next())
+console.log(iterateText.next())
