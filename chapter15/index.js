3b8113636430971b86b4afc9ab92867e777c04db
rahat854 <rahatkabir20@gmail.com>
2021-06-24 22:09:12 +0600

    custom iterator

--- chapter15/index.js
+++ chapter15/index.js
@@ -183,28 +183,28 @@ const arr = [1, 2, 3]
 // for(let v of 'Rahat Kabir') {
 //     console.log(v)
 // }
-let obj = {
-    start: 1,
-    end:5,
-    // [Symbol.iterator] : function() {
-    //     let currentValue = this.start
-    //     const self = this
-    //     return {
-    //         next() {
-    //             return {
-    //                 done: currentValue > self.end,
-    //                 value: currentValue > self.end ? undefined : currentValue++
-    //             }
-    //         }
-    //     }
-    // }
-    [Symbol.iterator] : function* () {
-        let currentValue = this.start
-        while(currentValue <= this.end) {
-            yield currentValue++
-        }
-    }
-}
+// let obj = {
+//     start: 1,
+//     end:5,
+//     // [Symbol.iterator] : function() {
+//     //     let currentValue = this.start
+//     //     const self = this
+//     //     return {
+//     //         next() {
+//     //             return {
+//     //                 done: currentValue > self.end,
+//     //                 value: currentValue > self.end ? undefined : currentValue++
+//     //             }
+//     //         }
+//     //     }
+//     // }
+//     [Symbol.iterator] : function* () {
+//         let currentValue = this.start
+//         while(currentValue <= this.end) {
+//             yield currentValue++
+//         }
+//     }
+// }
 
 // let iterate = obj[Symbol.iterator]()
 // console.log(iterate.next())
@@ -219,14 +219,47 @@ let obj = {
 //     yield 2
 //     yield 3
 // }
-function* generate(collection) {
-    for (let i = 0; i < collection.length; i++) {
-        yield collection[i]
-    }
-}
+// function* generate(collection) {
+//     for (let i = 0; i < collection.length; i++) {
+//         yield collection[i]
+//     }
+// }
 
-let it = generate(arr)
-console.log(it.next())
-console.log(it.next())
-console.log(it.next())
-console.log(it.next())
+// let it = generate(arr)
+// console.log(it.next())
+// console.log(it.next())
+// console.log(it.next())
+// console.log(it.next())
+
+// let obj = {
+//     start: 1,
+//     end: 10,
+//     [Symbol.iterator] : function () {
+//         let currentValue = this.start
+//         const self = this
+//         return {
+//             next() {
+//                 return {
+//                     done: currentValue > self.end,
+//                     value:currentValue > self.end ? undefined : currentValue++
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// // for(let v of obj) {
+// //     console.log(v)
+// // }
+// let iterate = obj[Symbol.iterator]()
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
