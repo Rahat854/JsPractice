fa4431d6cfdd469ce26602623c3fb34bb9a25e17
rahat854 <rahatkabir20@gmail.com>
2021-06-26 02:46:55 +0600

    inheritence in es6

--- chapter15/index.js
+++ chapter15/index.js
@@ -404,38 +404,62 @@
 // console.log(Object.getOwnPropertySymbols(c1))
 // let key = Object.getOwnPropertySymbols(c1)[1]
 // console.log(key)
-const _radius = new WeakMap()
-const _name = new WeakMap()
-const _resize = new WeakMap()
-class Circle {
-    constructor(radius, name) {
-        this.size = 100
-        _radius.set(this,radius)
-        _name.set(this, name)
-        _resize.set(this, () => {
-            console.log(this.size)
-        })
-    }
-    get radius() {
-        return _radius.get(this)
-    }
-    set radius(v) {
-        _radius.set(this,v)
-    }
+// const _radius = new WeakMap()
+// const _name = new WeakMap()
+// const _resize = new WeakMap()
+// class Circle {
+//     constructor(radius, name) {
+//         this.size = 100
+//         _radius.set(this,radius)
+//         _name.set(this, name)
+//         _resize.set(this, () => {
+//             console.log(this.size)
+//         })
+//     }
+//     get radius() {
+//         return _radius.get(this)
+//     }
+//     set radius(v) {
+//         _radius.set(this,v)
+//     }
 
+//     draw() {
+//         console.log('Drawing...')
+//         console.log(_radius.get(this), _name.get(this))
+//         _resize.get(this)()
+//     }
+// }
+// let c1 = new Circle(2, 'red')
+// console.log(c1)
+
+// console.log(Object.getOwnPropertyNames(c1))
+// console.log(Object.getOwnPropertySymbols(c1))
+// let key = Object.getOwnPropertySymbols(c1)[1]
+// console.log(key)
+// c1.draw()
+// c1.radius =  120
+// console.log(c1.radius)
+
+class Shape {
+    constructor(color) {
+        this.color = color
+    }
     draw() {
         console.log('Drawing...')
-        console.log(_radius.get(this), _name.get(this))
-        _resize.get(this)()
     }
 }
-let c1 = new Circle(2, 'red')
-console.log(c1)
 
-console.log(Object.getOwnPropertyNames(c1))
-console.log(Object.getOwnPropertySymbols(c1))
-let key = Object.getOwnPropertySymbols(c1)[1]
-console.log(key)
-c1.draw()
-c1.radius =  120
-console.log(c1.radius)
+class Rectangle extends Shape{
+    constructor (color,width, height) {
+        super(color)
+        this.width = width
+        this.height =  height
+    }
+
+    calculate() {
+        return this.width * this.height
+    }
+} 
+let r = new Rectangle('purple',4, 5)
+console.log(r)
+r.draw()
