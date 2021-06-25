1986e7765929990a67c1a9b905138fe86c5f9562
rahat854 <rahatkabir20@gmail.com>
2021-06-26 02:18:54 +0600

    class properties

--- chapter15/index.js
+++ chapter15/index.js
@@ -358,6 +358,10 @@ class Person{
     print() {
         console.log(this.name, this.email)
     }
+
+    test() {
+        console.log(this)
+    }
     static create(str) {
         let person =JSON.parse(str)
         return new Person(person.name, person.email)
@@ -369,3 +373,15 @@ let p1 = Person.create(str)
 console.log(p1)
 console.log(p1 instanceof Person)
 p1.print()
+
+function Shape() {
+    this.draw = function() {
+        console.log(this)
+    }
+}
+// let s1 =  new Shape()
+// let anotherDraw = s1.draw
+// anotherDraw()
+
+let test = p1.test
+test()
