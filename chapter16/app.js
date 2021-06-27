3d46ef626d711364eb13ec60a48d04da16927a5a
rahat854 <rahatkabir20@gmail.com>
2021-06-27 13:37:08 +0600

    error handle

--- chapter16/app.js
+++ chapter16/app.js
@@ -1,1 +1,15 @@
 
+// let n = Number.parseInt('45')
+// console.log(n)
+
+function changToInt(v) {
+    let result = Number.parseInt(v)
+    if(!result) {
+        return 'Please Provide a value which is able to covert to integer'
+    }
+
+    return result
+}
+
+let result = changToInt('86')
+console.log(result)
