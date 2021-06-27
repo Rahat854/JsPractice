52698f88dd5c249fa04b9644705940e044804280
rahat854 <rahatkabir20@gmail.com>
2021-06-27 13:44:59 +0600

    catch block error handling

--- chapter16/app.js
+++ chapter16/app.js
@@ -2,14 +2,30 @@
 // let n = Number.parseInt('45')
 // console.log(n)
 
-function changToInt(v) {
-    let result = Number.parseInt(v)
-    if(!result) {
-        return 'Please Provide a value which is able to covert to integer'
+// function changToInt(v) {
+//     let result = Number.parseInt(v)
+//     if(!result) {
+//         return 'Please Provide a value which is able to covert to integer'
+//     }
+
+//     return result
+// }
+
+// let result = changToInt('86')
+// console.log(result)
+
+function makeWords(text) {
+    try {
+        let str = text.trim()
+        let words = str.split(' ')
+        return words
+    } catch(e) {
+        console.log('Please provide a valid text')
     }
 
-    return result
+
+    // return words
 }
 
-let result = changToInt('86')
-console.log(result)
+let words = makeWords(86)
+console.log(words)
