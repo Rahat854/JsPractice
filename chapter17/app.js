60a91e4cbd182ae4b70f2f13732c59a156e4c137
rahat854 <rahatkabir20@gmail.com>
2021-06-28 17:23:48 +0600

    beginning

---
+++ chapter17/app.js
@@ -0,0 +1,7 @@
+//Synchronous programming
+//basically means that you can only execute one thing at a time
+//Asynchronous programming
+/*means that you can execute multiple thing at a time and you
+don't have to finish executing the current thing in order to 
+move on to next one */
+
