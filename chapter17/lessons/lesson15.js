30dd74493541cf2b10f969eb0dbf048b59ae9f4c
rahat854 <rahatkabir20@gmail.com>
2021-07-01 12:34:57 +0600

    async

--- chapter17/lessons/lesson15.js
+++ chapter17/lessons/lesson15.js
@@ -1,24 +1,24 @@
-let asyncIterable = {
-    [Symbol.asyncIterator]() {
-        let i = 0
-        return {
-            next() {
-                if(i < 5) {
-                    return Promise.resolve({
-                        value: i++,
-                        done: false
-                    })
-                } else {
-                    return Promise.resolve({
-                        done: true
-                    })
-                }
-            }
-        }
-    }
-}
+// let asyncIterable = {
+//     [Symbol.asyncIterator]() {
+//         let i = 0
+//         return {
+//             next() {
+//                 if(i < 5) {
+//                     return Promise.resolve({
+//                         value: i++,
+//                         done: false
+//                     })
+//                 } else {
+//                     return Promise.resolve({
+//                         done: true
+//                     })
+//                 }
+//             }
+//         }
+//     }
+// }
 
-let iterate = asyncIterable[Symbol.asyncIterator]();
+// let iterate = asyncIterable[Symbol.asyncIterator]();
 
 // (async function () {
 //     console.log(await iterate.next())
@@ -33,8 +33,23 @@ let iterate = asyncIterable[Symbol.asyncIterator]();
 //     console.log(await iterate.next())
 
 // })()
-(async function () {
-   for await (let v of asyncIterable) {
-       console.log(v)
-   } 
+// (async function () {
+//    for await (let v of asyncIterable) {
+//        console.log(v)
+//    } 
+// })()
+
+//async generator
+
+async function* myAsyncGenerator() {
+    let i = 0
+    while(true) {
+        if(i > 5) return
+            yield await Promise.resolve(i++)
+    }
+};
+(async function() {
+    for await(let v of myAsyncGenerator()) {
+        console.log(v)
+    }
 })()
