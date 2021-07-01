3344d1b636991db283a9754ae0de8b19f82413d9
rahat854 <rahatkabir20@gmail.com>
2021-07-02 00:50:19 +0600

    input and check box element with event handling

--- chapter18/lessons/lesson17.js
+++ chapter18/lessons/lesson17.js
@@ -7,3 +7,27 @@ Name.addEventListener('keypress', function(event) {
        event.target.value = ''
    }
 })
+
+let skills = document.getElementsByName('skills');
+let result = document.getElementById('result')
+let checkedSkills =[];
+[...skills].forEach(skill => {
+    skill.addEventListener('change', function(event) {
+        if(event.target.checked) {
+            checkedSkills.push(event.target.value)
+            outputskills(result, checkedSkills)
+        } else {
+            let ind = checkedSkills.indexOf(event.target.value)
+            checkedSkills.splice(ind, 1)
+            outputskills(result, checkedSkills)
+        }
+    })
+})
+
+function outputskills(parent, skills) {
+    let result = ''
+    skills.forEach((skill,index) => {
+        result += `(${index + 1}) ${skill}  `
+    })
+    parent.innerHTML = result
+}
