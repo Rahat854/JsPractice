87e8ac315cecd52db4ba81d23cd5a9f1d18f95df
rahat854 <rahatkabir20@gmail.com>
2021-07-01 14:19:17 +0600

    query selector

--- chapter18/lessons/lesson4.js
+++ chapter18/lessons/lesson4.js
@@ -1,13 +1,26 @@
 //Id, Class, Tag, Name
 
-let title = document.getElementById('title')
-console.log(title)
+// let title = document.getElementById('title')
+// console.log(title)
 
-let p = document.getElementsByClassName('lead')
+// let p = document.getElementsByClassName('lead')
+// console.log(p)
+
+// let t = document.getElementsByTagName('li')
+// console.log(t)
+
+// let n = document.getElementsByName('list-item-one')
+// console.log(n)
+
+let title = document.querySelector('#title')
+console.log(title)
+let p = document.querySelector('.lead')
 console.log(p)
 
-let t = document.getElementsByTagName('li')
-console.log(t)
+let p2 = document.querySelectorAll('.lead')
+console.log(p2)
 
-let n = document.getElementsByName('list-item-one')
+let t = document.querySelectorAll('li')
+console.log(t)
+let n = document.querySelector('[name ^= "list-item-one"]')
 console.log(n)
