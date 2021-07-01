000697ed75c8893e06ec8c14613658cca6fa0a04
rahat854 <rahatkabir20@gmail.com>
2021-07-01 21:47:40 +0600

    adjacent element

--- chapter18/lessons/lesson9.js
+++ chapter18/lessons/lesson9.js
@@ -4,13 +4,6 @@ let li = createElement('li', 'list-group-item','Four')
 let list = document.getElementById('list')
 list.appendChild(li)
 
-function createElement(tagName, className, innerHTML) {
-    let tag = document.createElement(tagName)
-    tag.innerHTML = innerHTML || ''
-    tag.className = className || ''
-    return tag
-}
-
 let p1 = createElement('p', 'lead', 'I am Rahat Kabir Dhrubo')
 let p2 = createElement('p', 'lead','I am Rahat Kabir Dhrubo' )
 
@@ -24,4 +17,11 @@ list.insertAdjacentElement('afterend',div)
 
 function append(parent,children) {
     children.forEach(child => parent.appendChild(child))
+}
+
+function createElement(tagName, className, innerHTML) {
+    let tag = document.createElement(tagName)
+    tag.innerHTML = innerHTML || ''
+    tag.className = className || ''
+    return tag
 }
