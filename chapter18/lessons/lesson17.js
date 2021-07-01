d54bcbdf30788079607729057d7cfc632f7696e5
rahat854 <rahatkabir20@gmail.com>
2021-07-02 01:03:27 +0600

    input, checkBox,list event handling

--- chapter18/lessons/lesson17.js
+++ chapter18/lessons/lesson17.js
@@ -31,3 +31,27 @@ function outputskills(parent, skills) {
     })
     parent.innerHTML = result
 }
+
+let list = document.getElementById('list')
+
+list.addEventListener('dblclick', function(event) {
+    if(this.contains(event.target)) {
+        let innerText = event.target.innerText
+        event.target.innerHTML = ''
+        let inputBox = creatInputBox(innerText)
+        event.target.appendChild(inputBox)
+        inputBox.addEventListener('keypress',function(e) {
+            if(e.key === 'Enter') {
+                event.target.innerHTML = e.target.value
+            }
+        })
+    }
+})
+
+function creatInputBox(value) {
+    let inp = document.createElement('input')
+    inp.type = 'text'
+    inp.className = 'form-control'
+    inp.value = value
+    return inp
+}
