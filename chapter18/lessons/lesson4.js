5f4cbde525088d17a535fe75f29aa9db10e26c51
rahat854 <rahatkabir20@gmail.com>
2021-07-01 14:09:16 +0600

    basic selector DOM

---
+++ chapter18/lessons/lesson4.js
@@ -0,0 +1,13 @@
+//Id, Class, Tag, Name
+
+let title = document.getElementById('title')
+console.log(title)
+
+let p = document.getElementsByClassName('lead')
+console.log(p)
+
+let t = document.getElementsByTagName('li')
+console.log(t)
+
+let n = document.getElementsByName('list-item-one')
+console.log(n)
