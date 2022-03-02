626337d424aba3748a3b639613364bc698449c5c
Rahat kabir <55030561+rahat664@users.noreply.github.com>
2022-03-02 21:07:36 +0600

    specify the built in functions

--- chapter2/math.js
+++ chapter2/math.js
@@ -4,14 +4,14 @@ console.log(Math.E)
 console.log(Math.PI)
 
 var n = 4.589
-console.log(Math.abs(n))
-console.log(Math.floor(n))
-console.log(Math.ceil(n))
-console.log(Math.round(n))
+console.log(Math.abs(n)) // log positive value
+console.log(Math.floor(n))// log the lower value 
+console.log(Math.ceil(n)) //log the heigher value
+console.log(Math.round(n)) // if less than .5 log lower value otherwise log higher value
 
-console.log(Math.max(8, 12 , 123, 412))
-console.log(Math.min(8, 12 , 123, 412))
-console.log(Math. pow(2,4))
-console.log(Math.sqrt(9))
+console.log(Math.max(8, 12 , 123, 412)) // log the max value
+console.log(Math.min(8, 12 , 123, 412)) // log the min value
+console.log(Math. pow(2,4)) // log base n 
+console.log(Math.sqrt(9)) // log base root
 
-console.log(Math.round((Math.random() * 50 + 1)))
+console.log(Math.round((Math.random() * 50 + 1))) // log random number
